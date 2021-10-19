4b7ccd8e728463856b6e8fbf4e07798ec7e3a218
surya9901 <surya9901@gmail.com>
2021-10-19 11:09:23 +0530

    Initial commit

--- src/card.js
+++ src/card.js
@@ -5,11 +5,11 @@ function Card(props) {
         <>
             <div className="col-lg-4">
                 <div className="card mt-5">
-                    <img className="card-img-top" src="https://assets.website-files.com/60131c7300605de7946be0d3/60131c7300605d6e776be117_Homepage_Results_Icon.png" alt="Card image cap" />
+                    <img className="card-img-top" src="http://placehold.it/110x80" alt="Card image cap" />
                     <div className="card-body">
                         <h5 className="card-title">{props.data.Name}</h5>
                         <p className="card-text">{props.data.Description}</p>
-                        <span class="badge badge-primary mr-1">Price: {props.data.Currency}{props.data.Price}</span>
+                        <span className="badge badge-primary mr-1">Price: {props.data.Currency}{props.data.Price}</span>
                         <a href="#" className="btn btn-success ml-1" 
                         onClick={() => props.handleclick(props.data.id)}>Add to Cart</a>
 
